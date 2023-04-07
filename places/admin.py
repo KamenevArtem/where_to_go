122efa90ed2725c2c8bd0ac193075fb06959c78f
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-04-07 04:35:46 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('place', 'number',)
+    list_editable = ['number']
+
 admin.site.register(Place, PlaceAdmin)
+admin.site.register(Image, ImageAdmin)
--- places/models.py
+++ places/models.py
@@ -2,8 +2,38 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField('Название места', max_length=200)
+    title = models.CharField(
+        'Название места',
+        max_length=200
+        )
     description_short = models.TextField('Краткое описание')
     description_long = models.TextField('Полное описание')
-    lng = models.FloatField('Долгота' ,null=True)
-    lat = models.FloatField('Широта' ,null=True)
+    lng = models.FloatField(
+        'Долгота',
+        null=True
+        )
+    lat = models.FloatField(
+        'Широта',
+        null=True
+        )
+    
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        verbose_name='Место',
+        related_name='places',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        db_index=True
+        )
+    image = models.ImageField('Файл картинки')
+    number = models.IntegerField(
+        'Номер картинки',
+        null=True
+        )
+
