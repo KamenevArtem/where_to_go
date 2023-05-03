dae6ca74c7dca9b5d44044e68f2c34da39a09031
KamenevArtem <kamenevart94@gmail.com>
2023-05-04 02:38:50 +0300

    Add api json response

--- places/views.py
+++ places/views.py
@@ -1,14 +1,28 @@
 import json
 import pathlib
 
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Place, Image
 
 
 def show_place(request, id):
     place = get_object_or_404(Place, id=id)
-    return HttpResponse(place.title)
+    images = place.images.all()
+    details = {
+        "title": place.title,
+        "imgs": [img.image.url for img in images],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": f"{place.lat}",
+            "lng": f"{place.lng}"
+        }
+    }
+    return JsonResponse(
+      details,
+      json_dumps_params={'ensure_ascii': False, 'indent': 4},
+      )
 
 
 def index(request):
@@ -17,19 +31,6 @@ def index(request):
     places = Place.objects.all()
     features = []
     for place in places:
-        images = place.images.all()
-        details = {
-            "title": place.title,
-            "imgs": [img.image.url for img in images],
-            "description_short": place.description_short,
-            "description_long": place.description_long,
-            "coordinates": {
-                "lat": f"{place.lat}",
-                "lng": f"{place.lng}"
-            }
-        }
-        with open (places_path.joinpath(f'{place.id}.json'), 'w', encoding='utf8') as json_file:
-            json.dump(details, json_file, ensure_ascii=False, indent=4)
         feature = {
             "type": "Feature",
             "geometry": {
