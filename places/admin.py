3599cfc86ec33dd12783058291c95cfd1d8004b4
KamenevArtem <kamenevart94@gmail.com>
2023-05-04 16:04:12 +0300

    add images preview to admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
-class ImageInline(admin.TabularInline):
+class Preview:
+    @staticmethod
+    def show_preview(obj):
+        return format_html(
+            '<img style="max-height:{height}" src="{url}"/>',
+            height='200px',
+            url=obj.image.url
+        )
+
+
+class ImageInline(admin.TabularInline, Preview):
     model = Image
-    fields = ['number', 'place', 'image']
+    fields = ['number', 'place', 'show_preview', 'image']
+    readonly_fields = ['show_preview']
     extra = 5
 
 
@@ -15,6 +27,7 @@ class PlaceAdmin(admin.ModelAdmin):
     ]
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('place', 'number',)
+class ImageAdmin(admin.ModelAdmin, Preview):
+    list_display = ('place', 'number', 'show_preview')
     list_editable = ['number']
+    readonly_fields = ['show_preview']
