6bdb9190876494d550e4de64cf672738f0ba4b98
KamenevArtem <kamenevart94@gmail.com>
2023-05-05 05:01:24 +0300

    rewrite image preview in admin panel

--- places/admin.py
+++ places/admin.py
@@ -4,9 +4,12 @@ from django.utils.html import format_html
 from .models import Place, Image
 
 
-class PreviewMixin:
-    @staticmethod
-    def show_preview(obj):
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ['place', 'image', 'show_preview']
+    readonly_fields = ['show_preview']
+    extra = 5
+    def show_preview(self, obj):
         return format_html(
             '<img style="max-height:{height}" src="{url}"/>',
             height='200px',
@@ -14,21 +17,21 @@ class PreviewMixin:
         )
 
 
-class ImageInline(SortableTabularInline, PreviewMixin):
-    model = Image
-    fields = ['place', 'show_preview', 'image']
-    readonly_fields = ['show_preview']
-    extra = 5
-
-
 @admin.register(Place)
 class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
+    fields = ('title', 'description_short', 'description_long', 'lng', 'lat')
     list_display = ('title', 'description_short', 'lng', 'lat')
     list_editable = ['description_short', 'lng', 'lat']
-    pass
+    inlines = [ImageInline]
 
 
 @admin.register(Image)
-class ImageAdmin(SortableAdminMixin, admin.ModelAdmin, PreviewMixin):
+class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ('id', 'show_preview', 'place', 'number')
     readonly_fields = ['show_preview']
+    def show_preview(self, obj):
+        return format_html(
+            '<img style="max-height:{height}" src="{url}"/>',
+            height='200px',
+            url=obj.image.url
+        )
