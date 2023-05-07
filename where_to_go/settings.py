c93592fd5dd748c4d07f6a47533593ae34f8a19c
KamenevArtem <kamenevart94@gmail.com>
2023-05-07 15:42:13 +0300

    static root setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -109,9 +109,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
-
-STATICFILES_DIRS = ["./static", ] 
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 
